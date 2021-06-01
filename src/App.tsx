8e386e2d22c3591762d3abdebf060f14cedc0c29
unknown <rivisrihard@gmail.com>
2021-06-01 18:00:53 +0300

    for pull

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,18 @@ import Home from './pages/Home';
 function App() {
   return (
     <div>
+      {/*
+      This is the router setup.
+      When the url changes to one of below routes, the page is
+      rerendered.
+
+      It is advised to put new pages on top of the code.
+      */}
       <Router>
         <Switch>
+          {/*
+          for example this renders the home page.
+          */}
           <Route path={"/"}>
             <Home />
           </Route>
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="nav-box">
+      <div className="link-box">
+        <div>1</div>
+        <div>2</div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
