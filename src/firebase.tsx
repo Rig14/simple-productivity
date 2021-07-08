d6556a0538bb37226ba98412d8c146d8f38ff481
Rig <41113390+Rig14@users.noreply.github.com>
2021-07-08 19:20:53 +0300

    Update firebase.tsx
    T

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -6,13 +6,13 @@ import 'firebase/auth';
 // firestore rules wont allow not signed in users to read or write data
 // also it disables users to see or write to other users data
 const config = {
-  apiKey: 'AIzaSyAuPDsjJODwFE-O4iPBpUGaYP1q2BMmfHk',
-  authDomain: 'simpleproductivity-bac4e.firebaseapp.com',
-  projectId: 'simpleproductivity-bac4e',
-  storageBucket: 'simpleproductivity-bac4e.appspot.com',
-  messagingSenderId: '92258693469',
-  appId: '1:92258693469:web:8d6453b0ef660339c578b4',
-  measurementId: 'G-PQ6RV7XGEY',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: '',
 };
 
 // initialize firebase
